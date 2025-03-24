eb586c31f98b911ff463729b8799a82e08e2bdde
KaitoMizukami <kaito.program@gmail.com>
2025-03-24 22:56:30 +0900

    initial commit

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/KaitoMizukami/twitter-like
+
+go 1.23.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
---
+++ backend/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	http.ListenAndServe(":8080", r)
+}
